cbe69c1d0f8e58556a4b835ab35c041f30ef6635
JavierGitHub20 <74763683+JavierGitHub20@users.noreply.github.com>
2025-01-26 16:13:37 +0000

    Deploying to gh-pages from @ JavierGitHub20/react-resume@6b44ae349842490d912f1de96ae7533453e48b4e 🚀

---
+++ precache-manifest.d1499c855087e1e6083520c07688ee74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d736321761fb03cca518a233c1193832",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "d45ecd52799e73589641",
+    "url": "/react-resume/static/css/main.9bc25216.chunk.css"
+  },
+  {
+    "revision": "c59157351cff9f37e258",
+    "url": "/react-resume/static/js/2.18518025.chunk.js"
+  },
+  {
+    "revision": "40bdc92e9f3e7d770136",
+    "url": "/react-resume/static/js/3.1ef5ba18.chunk.js"
+  },
+  {
+    "revision": "d45ecd52799e73589641",
+    "url": "/react-resume/static/js/main.53722068.chunk.js"
+  },
+  {
+    "revision": "2b40467b24e265a5ed00",
+    "url": "/react-resume/static/js/runtime~main.1999e881.js"
+  }
+]);
